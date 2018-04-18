57dc09bb96534f636dc9c8e394021770c91d2d34
Isaiah Weaver <iswea1@morgan.edu>
2018-04-18 11:55:19 -0400

    added poster images

--- Flicks/MovieCell.swift
+++ Flicks/MovieCell.swift
@@ -13,6 +13,7 @@ class MovieCell: UITableViewCell {
     
     @IBOutlet weak var overviewLabel: UILabel!
     
+    @IBOutlet weak var posterImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
